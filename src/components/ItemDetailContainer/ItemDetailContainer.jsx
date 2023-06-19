3dddcc2b383e88e553d62f368e401eedcdebc25f
cshjdr <cristian.rios@fit.com.ar>
2023-06-19 18:42:06 -0300

    se mejora la estetica de las cards en el detalle de la compra

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -11,11 +11,12 @@ const ItemDetailContainer = () => {
       .then((json) => setProduct(json));
   }, [id]);
 
+  //console.log('product', product);
 
   const { title, category, description, image, price } = product;
 
   return (
-    <div className="flex-shrink-0 relative mt-5 mb-5 m-auto overflow-hidden bg-orange-300 rounded-lg max-w-xl shadow-lg">
+    <div className="max-w-3xl flex-shrink-0 relative mt-5 mb-5 m-auto overflow-hidden bg-orange-300 rounded-lg shadow-lg">
       <svg
         className="absolute bottom-0 left-0 mb-8"
         viewBox="0 0 375 283"
@@ -40,36 +41,42 @@ const ItemDetailContainer = () => {
           fill="black"
         />
       </svg>
-      <div className="relative pt-10 px-10 flex items-center justify-center">
-        <div
-          className="block absolute w-48 h-48 bottom-0 left-0 -mb-24 ml-3"
-          style={{
-            background: "radial-gradient(black, transparent 60%)",
-            transform: "rotate3d(0, 0, 1, 20deg) scale3d(1, 0.6, 1)",
-            opacity: "0.2",
-          }}
-        ></div>
-        <img
-          src={image}
-          className="relative w-40"
-          alt="Imagen Anteojo Producto"
-        />
-      </div>
-      <div className="relative text-white px-6 pb-6 mt-6">
-        <span className="block opacity-75 mb-1">{category}</span>
-        <div className="block">
-          <span className="block font-semibold text-xl mb-1">{title}</span>
-          <span className="block text-xs">{description}</span>
-          <div className="mt-5 flex justify-between flex-wrap">
-            <span className="bg-white rounded-full text-black text-xs font-bold px-3 py-2 leading-none flex items-end h-6 w-auto">
-              <Link to="/">Volver a la tienda</Link>
-            </span>
-            <span className="bg-white rounded-full text-black text-xs font-bold px-3 py-2 leading-none flex items-end h-6 w-auto">
-              ${price}
-            </span>
-            <span className="bg-white rounded-full text-black text-xs font-bold px-3 py-2 leading-none flex items-end h-6 w-auto">
-              🛒 Comprar
-            </span>
+      <div className="flex flex-col md:flex-row">
+        <div className="w-full md:w-1/2">
+          <div className="relative pt-10 px-10 flex items-center justify-center">
+            <div
+              className="block absolute w-48 h-48 bottom-0 left-0 -mb-24 ml-3"
+              style={{
+                background: "radial-gradient(black, transparent 60%)",
+                transform: "rotate3d(0, 0, 1, 20deg) scale3d(1, 0.6, 1)",
+                opacity: "0.2",
+              }}
+            ></div>
+            <img
+              src={image}
+              className="relative w-full"
+              alt="Imagen Anteojo Producto"
+            />
+          </div>
+        </div>
+        <div className="w-full md:w-1/2">
+          <div className="relative text-white px-6 pb-6 mt-6">
+            <span className="block opacity-75 mb-1">{category}</span>
+            <div className="block">
+              <span className="block font-semibold text-xl mb-1">{title}</span>
+              <span className="block text-xs">{description}</span>
+              <div className="mt-5 flex justify-between flex-wrap">
+                <span className="bg-white rounded-full text-black text-xs font-bold px-3 py-2 leading-none flex items-end h-6 w-auto">
+                  ${price}
+                </span>
+                <span className="bg-white rounded-full text-black text-xs font-bold px-3 py-2 leading-none flex items-end h-6 w-auto">
+                  <Link to="/">Volver a la tienda</Link>
+                </span>
+                <span className="bg-white rounded-full text-black text-xs font-bold px-3 py-2 leading-none flex items-end h-6 w-auto">
+                  🛒 Comprar
+                </span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
